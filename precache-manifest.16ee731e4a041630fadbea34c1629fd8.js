1b4212dc20868e05d068b58671b523d5199d8c2e
Emma Walden <emmajanewalden@gmail.com>
2020-05-01 15:11:31 -0400

    Updates

---
+++ precache-manifest.16ee731e4a041630fadbea34c1629fd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93cbc56359bb62e796b276825bb8d9d",
+    "url": "/emmaWaldenProject5/index.html"
+  },
+  {
+    "revision": "bd83d01669425b2ca39a",
+    "url": "/emmaWaldenProject5/static/css/main.90a59fd4.chunk.css"
+  },
+  {
+    "revision": "2be552e1c2f70a0f8802",
+    "url": "/emmaWaldenProject5/static/js/2.aa73642c.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/emmaWaldenProject5/static/js/2.aa73642c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd83d01669425b2ca39a",
+    "url": "/emmaWaldenProject5/static/js/main.d7384d1f.chunk.js"
+  },
+  {
+    "revision": "56dd6377a3d5e1b772f7",
+    "url": "/emmaWaldenProject5/static/js/runtime-main.2ce94621.js"
+  },
+  {
+    "revision": "8bd6d8ba7b04e8ea9fc729a1a33de706",
+    "url": "/emmaWaldenProject5/static/media/book.8bd6d8ba.svg"
+  },
+  {
+    "revision": "d0301819ef7f3897ae73ef78347e69c8",
+    "url": "/emmaWaldenProject5/static/media/close.d0301819.svg"
+  }
+]);
